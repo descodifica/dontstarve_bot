cc95631719fcddefc282fbf0b5bb704bb4518bcb
eudiasrafael <eu@diasrafael.com.br>
2020-07-06 14:05:42 -0300

    Token privado

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+// Importa o token
+const token = require('./token')
+
 // Configurações do boot
 const config = {
-    token: 'NzI4MjQ3NjQ3OTAyNjI5OTI4.Xv4Ilw.N_CU0c6jhHLk1ZIXP8pfXnXECfM', // Token de acesso
+    token, // Token de acesso
     prefix: 'ds:', // Prefixo padrão
     Db: { // Conexão com o banco
         host: 'localhost',
