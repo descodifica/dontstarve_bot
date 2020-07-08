c5951e2477372029600694f861d65c25a2cbe1ee
eudiasrafael <eu@diasrafael.com.br>
2020-07-08 11:57:45 -0300

    Ajustado aviso de idioma faltando

--- resolveLangMessage.js
+++ resolveLangMessage.js
@@ -2,7 +2,7 @@ const languages = [ 'ptbr', 'en', ]
 // Resolve uma mensagem em seu idioma correto
 module.exports = (_lang, _msgs = {}) => {
   languages.map(lang => {
-    if (_msgs[lang]) {
+    if (!_msgs[lang]) {
       console.log(`Faltando idioma ${lang}`)
     }
   })
