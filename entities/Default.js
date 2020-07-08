9b5fa1d7c38bbd84795552332c6a916a6c57928b
eudiasrafael <eu@diasrafael.com.br>
2020-07-08 11:58:22 -0300

    Melhorado sistema de log

--- entities/Default.js
+++ entities/Default.js
@@ -17,10 +17,11 @@ class Default {
   /**
     * @description Retorna um registro dado um Id
     * @param {String} _id Id do registro a ser recuperado
+    * @param {Boolean} _log Se deve imprimir o log
     * @returns {Object} O resultado
     */
-  async get (_id) {
-    const result = await this.getBy({ id: _id, })
+  async get (_id, _log) {
+    const result = await this.getBy({ id: _id, }, _log)
 
     return result[0]
   }
@@ -28,10 +29,13 @@ class Default {
   /**
     * @description Retorna um registro dado um ou mais condições
     * @param {Object} _where Condições
+    * @param {Boolean} _log Se deve imprimir o log
     * @returns {Object} O resultado
     */
-  async getBy (_where) {
-    const result = await Db.query(`SELECT * FROM ${this.table} WHERE ${jsonToSqlWhere(_where)}`)
+  async getBy (_where, _log) {
+    const result = await Db.query(
+      `SELECT * FROM ${this.table} WHERE ${jsonToSqlWhere(_where)}`, _log
+    )
 
     return result
   }
@@ -39,23 +43,25 @@ class Default {
   /**
     * @description Cria um novo registro
     * @param {Object} _data Dados a serem gravados
+    * @param {Boolean} _log Se deve imprimir o log
     * @returns {Object} O resultado
     */
-  create (_data) {
+  create (_data, _log) {
     const columns = Object.keys(_data).join(', ')
     const values = Object.values(_data).map(i => typeof i === 'string' ? `"${i}"` : i).join(', ')
 
     // Executa SQL de criação e retorna o resultado
-    return Db.query(`INSERT INTO ${this.table} ${columns} VALUES ${values}`)
+    return Db.query(`INSERT INTO ${this.table} (${columns}) VALUES (${values})`, _log)
   }
 
   /**
     * @description Atualiza um registro
     * @param {Object} _data Dados a serem gravados
     * @param {Object} _where Condições para gravar
+    * @param {Boolean} _log Se deve imprimir o log
     * @returns {Object} O resultado
     */
-  update (_data, _where) {
+  update (_data, _where, _log) {
     const data = []
 
     // Monta treixo de atualização da SQL
@@ -70,7 +76,9 @@ class Default {
     })
 
     // Executa SQL de atualização e retorna o resultado
-    return Db.query(`UPDATE ${this.table} SET ${data.join(', ')} WHERE ${jsonToSqlWhere(_where)}`)
+    return Db.query(
+      `UPDATE ${this.table} SET ${data.join(', ')} WHERE ${jsonToSqlWhere(_where)}`, _log
+    )
   }
 }
 
