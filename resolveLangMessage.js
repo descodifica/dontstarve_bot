31c0f07d5b9d8a1f0ed0239a68c653ab35cf67c1
eudiasrafael <eu@diasrafael.com.br>
2020-07-08 11:40:10 -0300

    resolveLangMessage agora informa no console se estiver faltando um idioma

--- resolveLangMessage.js
+++ resolveLangMessage.js
@@ -1,4 +1,11 @@
+const languages = [ 'ptbr', 'en', ]
 // Resolve uma mensagem em seu idioma correto
 module.exports = (_lang, _msgs = {}) => {
+  languages.map(lang => {
+    if (_msgs[lang]) {
+      console.log(`Faltando idioma ${lang}`)
+    }
+  })
+
   return _msgs[_lang]
 }
